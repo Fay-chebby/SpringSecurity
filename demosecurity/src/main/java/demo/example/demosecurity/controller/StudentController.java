5e9fc87d5bf1480bbf5f91362d84a4e693d749f1
Fai-chebby <chebetf034@gmail.com>
2024-04-28 22:11:24 +0300

    Security

--- demosecurity/src/main/java/demo/example/demosecurity/controller/StudentController.java
+++ demosecurity/src/main/java/demo/example/demosecurity/controller/StudentController.java
@@ -11,6 +11,10 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/students")
 public class StudentController {
+    private  static final List<Student> STUDENTS=Arrays.asList(
+        new Student(1,"Faith"),
+        new Student(2,"Rop")
+    );
 
     @GetMapping(path = "studentId")
     public Student getStudent(@PathVariable("studentId")  Integer studentId){
