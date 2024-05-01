d697f75992f2a3e6f52d4290161346ea92f197d7
Fai-chebby <chebetf034@gmail.com>
2024-05-01 17:16:36 +0300

    SpringSecurity

--- demosecurity/src/main/java/demo/example/demosecurity/security/ApplicationSecurityConf.java
+++ demosecurity/src/main/java/demo/example/demosecurity/security/ApplicationSecurityConf.java
@@ -8,8 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 public class ApplicationSecurityConf extends WebSecurityConfigurerAdapter {
     @Override
-    protected  void configure (HttpSecurity http) throws  Exception{
-        super.configure(http);
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 
 }
